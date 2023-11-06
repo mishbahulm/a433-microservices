d12ca426c19311bdd05894743a79a778584061d8
mishbahulm <munir.m3@gmail.com>
2023-11-06 20:45:43 +0700

    menambahkan circle config

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# build image
+docker build -t ghcr.io/mishbahulm/karsajobs-ui:latest .
+
+# login to github packages
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u mishbahulm --password-stdin
+
+# push image to github packages
+docker push ghcr.io/mishbahulm/karsajobs-ui:latest
